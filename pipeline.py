22531d2614322e7eb35346c2f242402ca9f1fabd
Alard <alard@example.com>
2013-01-12 12:59:16 +0100

    Typo in extract-users command line.

--- pipeline.py
+++ pipeline.py
@@ -26,7 +26,7 @@ if StrictVersion(seesaw.__version__) < StrictVersion("0.0.10"):
 
 
 USER_AGENT = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27"
-VERSION = "20130111.01"
+VERSION = "20130112.01"
 
 class ConditionalTask(Task):
   def __init__(self, condition_function, inner_task):
@@ -146,7 +146,7 @@ pipeline = Pipeline(
   ),
   ExternalProcess("User extraction", [
     "./extract-users.sh",
-    ItemInterpolation("%(data_dir)%s"),
+    ItemInterpolation("%(data_dir)s"),
     ItemInterpolation("%(data_dir)s/%(warc_file_base)s.users.txt")
   ]),
   PrepareStatsForTracker(
